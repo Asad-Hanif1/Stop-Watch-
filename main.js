cd926f763176378846bc13acdb29fc0ab2ecdd2d
Asad-Hanif1 <hanifasad617@gmail.com>
2024-04-26 15:23:19 +0500

    stop file created

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ function start() {
     startTime = Date.now();
     time = setInterval(updatedTime, 10);
     document.querySelector("result").innerHTML = time;
-    // console.log(time);
-    document.getElementById("start").disabled = true;
+   
+    document.querySelector("start").disabled = true;
 }
 
 function stop() {
     clearInterval(time);
     let stopTime = (Date.now() - startTime) / 1000;
     document.querySelector("result").innerHTML = stopTime.toFixed(2);
-    // console.log("Elapsed time: " + stopTime.toFixed(2) + " seconds");
-    document.getElementById("start").disabled = false;
+   
+    document.querySelector("start").disabled = false;
 }
 
 function updatedTime() {
     let stopTime = (Date.now() - startTime) / 1000;
     document.querySelector("result").innerHTML = stopTime.toFixed(2);
-    // console.log(stopTime.toFixed(2));
+    
 }
